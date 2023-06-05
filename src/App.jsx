d4433d632243e7cfbeb1c5200e43b8f69f1d56d5
hungchihsueh <84211143+hungchihsueh@users.noreply.github.com>
2023-06-05 18:33:29 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,45 +5,23 @@ import * as d3 from "d3";
 const reader = new FileReader();
 import { marked } from "marked";
 import mammoth from "mammoth";
+import mermaid from "mermaid";
 import renderer from "./renderer/markedRenderer";
 
-// // marked renderer
-// const renderer = new marked.Renderer();
-// renderer.heading = function (text, level) {
-// 	const escapedText = text.toLowerCase().replace(/[^\w]+/g, "-");
-
-// 	return `
-// 	<h${level} class="heading">
-// 	  ${text}
-// 	</h${level}>`;
-// };
-// renderer.code = function (code, language, isEscaped) {
-// 	let codeArr = code.split("\n");
-// 	console.log("codeArr", codeArr);
-// 	let convertedCodeArr = [];
-// 	for (let i = 0; i < codeArr.length; i++) {
-// 		// if code is markdown heading
-// 		if (codeArr[i].startsWith("#")) {
-// 			convertedCodeArr.push(`<h1>${codeArr[i]}</h1>`);
-// 		}
-// 		// if code is markdown image
-// 		else if (codeArr[i].startsWith("![")) {
-// 			// get the image url
-// 			let url = codeArr[i].match(/\(([^)]+)\)/)[1];
-// 			console.log("url", url);
-// 			convertedCodeArr.push(`<img src="${url}" >`);
-// 		}
-// 		// if code if markdown paragraph
-// 		else {
-// 			convertedCodeArr.push(`<p>${codeArr[i]}</p>`);
-// 		}
-// 	}
-
-// 	console.log("convertedCodeArr", convertedCodeArr);
-// 	return `<div class="${language}">${convertedCodeArr.join("")}}</div>`;
-// };
 marked.use({ renderer });
+
 function App() {
+	async function doMermaid() {
+		mermaid.initialize();
+		const mermaids = document.querySelectorAll(".mermaid");
+		console.log("mermaids", mermaids);
+		mermaids.forEach((m) => {
+			let graphDefinition = m.innerText;
+			mermaid.render("graphDiv", graphDefinition).then((svgObj) => {
+				m.innerHTML = svgObj.svg;
+			});
+		});
+	}
 	function convertToHTML() {
 		var fileInput = document.getElementById("fileInput");
 		var file = fileInput.files[0];
@@ -75,7 +53,7 @@ function App() {
 						return trimmedSections;
 					}
 					const sections = separateMarkdownSections(html);
-					console.log(sections);
+					console.log("sections", sections);
 					document.getElementById("output").innerText = html;
 
 					let newArr = [...sections];
@@ -98,7 +76,7 @@ function App() {
 						) {
 							codeBlockStartIndex = i;
 							codeBlock.push(newArr[i]);
-							console.log("codeBlock=0", codeBlock);
+							// console.log("codeBlock=0", codeBlock);
 						} else {
 							if (
 								typeof newArr[i] != "object" &&
@@ -106,9 +84,9 @@ function App() {
 								newArr[i].endsWith("```")
 							) {
 								codeBlock.push(newArr[i]);
-								console.log("codeBlock!!!", codeBlock);
+								// console.log("codeBlock!!!", codeBlock);
 								newArr.splice(codeBlockStartIndex, codeBlock.length, codeBlock);
-								console.log("newArr", newArr);
+								// console.log("newArr", newArr);
 								i = 0;
 								codeBlockStartIndex = 0;
 								codeBlock = [];
@@ -118,7 +96,7 @@ function App() {
 								!newArr[i].endsWith("```")
 							) {
 								codeBlock.push(newArr[i]);
-								console.log("codeBlock!=0", codeBlock);
+								// console.log("codeBlock!=0", codeBlock);
 							}
 						}
 					}
@@ -135,6 +113,7 @@ function App() {
 					x = x.join("");
 					// console.log(x);
 					document.getElementById("output").innerHTML = x;
+					doMermaid();
 				})
 				.done();
 		};
@@ -158,98 +137,10 @@ function App() {
 					onClick={() => convertToHTML()}>
 					Convert to HTML
 				</button>
-				<button
-					onClick={() => {
-						marked.use({ renderer });
-						let asd = [
-							"# 標題",
-							"本集團以「成為國際永續標竿企業，積極為後代推動更好的未來」為願景，面對人類本世紀最大挑戰之一的氣候變...候風險並採取積極策略，發揮資金提供者與管理者的影響力，驅動價值鏈低碳轉型，朝向淨零碳排的最終目標。",
-							"本報告書透過以下四面向闡述本集團氣候相關風險與機會管理作為，以展現本集團對於氣候變遷減緩與調適之承諾。",
-							"```talk",
-							"# some heading",
-							"# some heading 2",
-							"paragraph1",
-							"paragraph2asdfklhjjjjlljjjjjjjjjjjjjjjjjjjjjjjjllj…dhfkjsdnf,mn,mnvxc,mnvxcm,vnxcm,nwehfkjwhfwkejfhw",
-							"paragraph3",
-							"![](https://images.unsplash.com/photo-168537186362…DB8fHx8fA%3D%3D&auto=format&fit=crop&w=2787&q=80)",
-							"```",
-							"random sutff",
-							"```www",
-							"# some heading123",
-							"# some heading 1242",
-							"paragraph13423",
-							"paragraph2asdfklhjjjjlljjjjjjjjjjjjjjjjjjjjjjjjllj…dhfkjsdnf,mn,mnvxc,mnvxcm,vnxcm,nwehfkjwhfwkejfhw",
-							"paragraph233",
-							"![](https://images.unsplash.com/photo-168537186362…DB8fHx8fA%3D%3D&auto=format&fit=crop&w=2787&q=99980)",
-							"```",
-						];
-						let newArr = [...asd];
-						// console.log(newArr);
-						let codeBlockStartIndex = 0;
-						let codeBlock = [];
-						// know the code block start with "```SOMETHONG" and end with "```"
-						// try identify where "```SOMETHING" at and where "```" at
-						// then try totake the item between index and put it into a new array
-						//and put it back to the original array at the same index
 
-						for (let i = 0; i < newArr.length; i++) {
-							// console.log(codeBlockStartIndex);
-							// console.log("newArr[i]", newArr[i]);
-
-							if (
-								typeof newArr[i] != "object" &&
-								codeBlockStartIndex == 0 &&
-								newArr[i].startsWith("```")
-							) {
-								codeBlockStartIndex = i;
-								codeBlock.push(newArr[i]);
-								console.log("codeBlock=0", codeBlock);
-							} else {
-								if (
-									typeof newArr[i] != "object" &&
-									codeBlockStartIndex !== 0 &&
-									newArr[i].endsWith("```")
-								) {
-									codeBlock.push(newArr[i]);
-									console.log("codeBlock!!!", codeBlock);
-									newArr.splice(
-										codeBlockStartIndex,
-										codeBlock.length,
-										codeBlock,
-									);
-									console.log("newArr", newArr);
-									i = 0;
-									codeBlockStartIndex = 0;
-									codeBlock = [];
-								} else if (
-									typeof newArr[i] != "object" &&
-									codeBlockStartIndex !== 0 &&
-									!newArr[i].endsWith("```")
-								) {
-									codeBlock.push(newArr[i]);
-									console.log("codeBlock!=0", codeBlock);
-								}
-							}
-						}
-						console.log("the end", newArr);
-
-						let x = newArr.flatMap((item) => {
-							if (typeof item == "object") {
-								// console.log("item", marked(item.join("\n")));
-								return marked(`${item.join("\n")}`);
-							}
-							return marked(item);
-						});
-						// console.log("before join", x);
-						x = x.join("");
-						// console.log(x);
-						document.getElementById("output").innerHTML = x;
-					}}>
-					test
-				</button>
 				<div
 					id="output"
-					className="container"></div>
+					className="mx-auto w-[1190px]"></div>
 			</div>
 		</>
 	);
--- src/renderer/markedRenderer.js
+++ src/renderer/markedRenderer.js
@@ -1,38 +1,139 @@
-import {marked} from "marked";
+import { marked } from "marked";
+import { render } from "react-dom";
+import mermaid from "mermaid";
 // marked renderer
 const renderer = new marked.Renderer();
 renderer.heading = function (text, level) {
 	const escapedText = text.toLowerCase().replace(/[^\w]+/g, "-");
+	return `<h${level} class="heading">${text}</h${level}>`;
+};
 
-	return `
-	<h${level} class="heading">
-	  ${text}
-	</h${level}>`;
+renderer.paragraph = function (text) {
+	return `<p>${text}</p>`;
+};
+renderer.image = function (href, title, text) {
+	return `<img src="${href}" alt="${text}"/>`;
 };
 renderer.code = function (code, language, isEscaped) {
-	let codeArr = code.split("\n");
-	console.log("codeArr", codeArr);
-	let convertedCodeArr = [];
-	for (let i = 0; i < codeArr.length; i++) {
-		// if code is markdown heading
-		if (codeArr[i].startsWith("#")) {
-			convertedCodeArr.push(`<h1>${codeArr[i]}</h1>`);
-		}
-		// if code is markdown image
-		else if (codeArr[i].startsWith("![")) {
-			// get the image url
-			let url = codeArr[i].match(/\(([^)]+)\)/)[1];
-			console.log("url", url);
-			convertedCodeArr.push(`<img src="${url}" >`);
-		}
-		// if code if markdown paragraph
-		else {
-			convertedCodeArr.push(`<p>${codeArr[i]}</p>`);
-		}
-	}
 
-	console.log("convertedCodeArr", convertedCodeArr);
-	return `<div class="${language}">${convertedCodeArr.join("")}}</div>`;
+	// console.log("code", code);
+	let codeArr = code.split("\n");
+	// console.log("codeArr", codeArr);
+	let convertedCodeArr = [];
+
+
+		switch (language) {
+			case "talk":
+				for (let i = 0; i < codeArr.length; i++) {
+					convertedCodeArr.push(marked(codeArr[i]));
+					// // if code is markdown heading
+					// if (codeArr[i].startsWith("#")) {
+					// 	// remove the # from the heading
+					// 	codeArr[i] = codeArr[i].replace("#", "");
+					// 	convertedCodeArr.push(`<h1>${codeArr[i]}</h1>`);
+					// }
+					// // if code is markdown image
+					// else if (codeArr[i].startsWith("![")) {
+					// 	// get the image url
+					// 	let url = codeArr[i].match(/\(([^)]+)\)/)[1];
+					// 	console.log("url", url);
+					// 	convertedCodeArr.push(`<img src="${url}" >`);
+					// }
+					// // if code if markdown paragraph
+					// else {
+					// 	convertedCodeArr.push(`<p>${codeArr[i]}</p>`);
+					// }
+				}
+				let paragraphArr = [];
+				// console.log("convertedCodeArr", convertedCodeArr);
+				for (let i = 0; i < convertedCodeArr.length; i++) {
+					if (
+						(convertedCodeArr[i].startsWith("<p") ||
+							convertedCodeArr[i].startsWith("<h1")) &&
+						convertedCodeArr[i].startsWith("<p><img") == false
+					) {
+						paragraphArr.push(convertedCodeArr[i]);
+					} else {
+						break;
+					}
+				}
+				// console.log("paragraphArr", paragraphArr);
+				// an array call imgArr that is the rest of the convertedCodeArr without the paragraphArr
+				convertedCodeArr.splice(0, paragraphArr.length);
+				let imgArr = convertedCodeArr;
+
+				let textPart =
+					'<div class="textPart">' + paragraphArr.join("") + "</div>";
+				let imgPart = "<div>" + imgArr.join("") + "</div>";
+
+				return `<div class="${language}">${textPart}${imgPart}</div>`;
+				break;
+			case "mermaid":
+				// mermaid.initialize();
+				// mermaid
+				// 	.render("graphDiv", code)
+				// 	.then((svgCode) => {
+				// 		console.log("Rendered!", svgCode.svg);
+				// 		return svgCode.svg;
+				// 	})
+				// return
+				if (code.match(/^sequenceDiagram/) || code.match(/^graph/)|| code.match(/^flowchart/)) {
+					return '<pre class="mermaid">' + code + '</pre>';
+				} else {
+					return '<pre><code>' + code + '</code></pre>';
+				}
+				break;
+			default:
+				return `<pre><code class="${language}">${code}</code></pre>`;
+		}
+
+	// if (language === "talk") {
+	// 	let codeArr = code.split("\n");
+	// 	let convertedCodeArr = [];
+	// 	for (let i = 0; i < codeArr.length; i++) {
+	// 		console.log("codeArr[i]", marked(codeArr[i]));
+	// 		convertedCodeArr.push(marked(codeArr[i]));
+	// 		// // if code is markdown heading
+	// 		// if (codeArr[i].startsWith("#")) {
+	// 		// 	// remove the # from the heading
+	// 		// 	codeArr[i] = codeArr[i].replace("#", "");
+	// 		// 	convertedCodeArr.push(`<h1>${codeArr[i]}</h1>`);
+	// 		// }
+	// 		// // if code is markdown image
+	// 		// else if (codeArr[i].startsWith("![")) {
+	// 		// 	// get the image url
+	// 		// 	let url = codeArr[i].match(/\(([^)]+)\)/)[1];
+	// 		// 	console.log("url", url);
+	// 		// 	convertedCodeArr.push(`<img src="${url}" >`);
+	// 		// }
+	// 		// // if code if markdown paragraph
+	// 		// else {
+	// 		// 	convertedCodeArr.push(`<p>${codeArr[i]}</p>`);
+	// 		// }
+	// 	}
+	// 	let paragraphArr = [];
+	// 	console.log("convertedCodeArr", convertedCodeArr);
+	// 	for (let i = 0; i < convertedCodeArr.length; i++) {
+	// 		if (
+	// 			(convertedCodeArr[i].startsWith("<p") ||
+	// 				convertedCodeArr[i].startsWith("<h1")) &&
+	// 			convertedCodeArr[i].startsWith("<p><img") == false
+	// 		) {
+	// 			paragraphArr.push(convertedCodeArr[i]);
+	// 		} else {
+	// 			break;
+	// 		}
+	// 	}
+	// 	console.log("paragraphArr", paragraphArr);
+	// 	// an array call imgArr that is the rest of the convertedCodeArr without the paragraphArr
+	// 	convertedCodeArr.splice(0, paragraphArr.length);
+	// 	let imgArr = convertedCodeArr;
+
+	// 	let textPart = '<div class="textPart">' + paragraphArr.join("") + "</div>";
+	// 	let imgPart = "<div>" + imgArr.join("") + "</div>";
+
+	// 	return `<div class="${language}">${textPart}${imgPart}</div>`;
+	// }
 };
 
 export default renderer;
